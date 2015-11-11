7c22ff514b4ab097f22a6f54ce6c644383bf5a30
simran1292 <ssimran1292@gmail.com>
2015-11-10 22:12:01 -0500

    function added.

--- Script/coding.js
+++ Script/coding.js
@@ -1,2 +1,9 @@
 var ac = 'cover.jpg';
 var orignal = ['1.jpg','2.jpg','3.jpg','4.jpg','5.jpg'];
+function coverpic(i) 
+{
+    document.getElementById('getcover'+i).innerHTML = '<div onclick="displayOrignal('+i+');return false;"><img src="'+ac+'" width="200" height="200" \/><\/div>';
+    
+} 
+var c, d,No, tableid, timerss; 
+window.onload=run;
