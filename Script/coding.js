b876f0367be798063efbbcb4f6babd4e2f07c5f2
simran1292 <ssimran1292@gmail.com>
2015-11-10 22:56:34 -0500

    changes made

--- Script/coding.js
+++ Script/coding.js
@@ -1,14 +1,12 @@
 var ac = 'cover.jpg';
 var orignal = ['1.jpg','2.jpg','3.jpg','4.jpg','5.jpg'];
-
 function coverpic(i) 
 {
     document.getElementById('getcover'+i).innerHTML = '<div onclick="displayOrignal('+i+');return false;"><img src="'+ac+'" width="200" height="200" \/><\/div>';
     
 } 
 var c, d,No, tableid, timerss; 
-window.onload=run;
-
+window.onload=run; 
 function NumberRandom(a, b)
 {
     return (Math.round(Math.random())-0.3);
@@ -21,7 +19,8 @@ function run()
         clearInterval(tableid);
         No = 0;
         orignal.sort( NumberRandom );
-} 
+    } 
+}
 function displayOrignal(data) 
 {
     if (No>1)
@@ -61,3 +60,4 @@ function validate()
         coverpic(d);
     } 
 }
+                    
